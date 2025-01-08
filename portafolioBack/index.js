78a4e5c805b1801e00c4966e592fa05e443934c4
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-01-08 18:18:30 +0100

    Se crean los modelos a trabajar en mongoose

--- portafolioBack/index.js
+++ portafolioBack/index.js
@@ -1,9 +1,12 @@
 // Server creation and configuration
-const http = require('http');
-const app = require('./src/app');
+const http = require("http");
+const app = require("./src/app");
 
 // Config .env
-require('dotenv').config();
+require("dotenv").config();
+
+// Cofifuracion BD
+require("./src/config/db");
 
 // Server creation
 const server = http.createServer(app);
@@ -12,10 +15,10 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT);
 
 // Listeners
-server.on('listening', () => {
-    console.log(`Server listening on port ${PORT}`);
+server.on("listening", () => {
+  console.log(`Server listening on port ${PORT}`);
 });
 
-server.on('error', (error) => {
-    console.log(error);
+server.on("error", (error) => {
+  console.log(error);
 });
---
+++ portafolioBack/src/config/db.js
@@ -0,0 +1,5 @@
+// Requerimos la libreria
+const mongoose = require("mongoose");
+
+// Configuramos la conexión a la base de datos
+mongoose.connect("mongodb://localhost:27017");
